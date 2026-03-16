dfa60a67fce659d844b23bc9ef747b418e9714f5
Henrique Teruya <henriqueteruya12@gmail.com>
2026-03-16 11:39:40 -0300

    alinhamento dos botões e formatação no dashboard e atualização do visual de login

--- functions/index.js
+++ functions/index.js
@@ -56,6 +56,70 @@ document.getElementById("copyBtn").addEventListener("click", function () {
     }
 });
 
+// Ação para a área Drag & Drop
+const dropArea = document.getElementById("dropArea");
+const fileInput = document.getElementById("file");
+const fileInfo = document.getElementById("fileInfo");
+const fileName = document.getElementById("fileName");
+const removeFileBtn = document.getElementById("removeFileBtn");
+
+// Clicar aciona o input escondido
+dropArea.addEventListener("click", function () {
+    fileInput.click();
+});
+
+dropArea.addEventListener("dragover", function (e) {
+    e.preventDefault();
+    dropArea.classList.add("dragover");
+});
+
+dropArea.addEventListener("dragleave", function () {
+    dropArea.classList.remove("dragover");
+});
+
+dropArea.addEventListener("drop", function (e) {
+    e.preventDefault();
+    dropArea.classList.remove("dragover");
+
+    if (e.dataTransfer.files.length > 0) {
+        fileInput.files = e.dataTransfer.files;
+        atualizarVisualArquivo();
+    }
+});
+
+fileInput.addEventListener("change", function () {
+    if (fileInput.files.length > 0) {
+        atualizarVisualArquivo();
+    }
+});
+
+function atualizarVisualArquivo() {
+    let name = fileInput.files[0].name;
+
+    if (name.length > 13) {
+        name = name.substring(0, 10) + "... .csv";
+    } else {
+        // Se for menor que 13/17 no total, preenchemos a string com "non-breaking spaces" (espaços invisíveis inquebráveis)
+        // O limite 18 equivale ao tamanho fixo gerado por: 10 letras + "... .csv" (8) = 18 caracteres totais
+        name = name.padEnd(18, '\u00A0');
+    }
+
+    fileName.textContent = name;
+    dropArea.style.display = "none";
+    fileInfo.style.display = "flex";
+}
+
+removeFileBtn.addEventListener("click", function () {
+    fileInput.value = "";
+    fileName.textContent = "";
+    fileInfo.style.display = "none";
+    dropArea.style.display = "block";
+
+    // Aproveita para limpar o textarea do JSON gerado do arquivo anterior
+    const resultElement = document.getElementById("result");
+    resultElement.value = "";
+    resultElement.rows = 3;
+});
 document.getElementById("clearBtn").addEventListener("click", function () {
     const resultElement = document.getElementById("result");
     resultElement.value = "";
